e623e0da1b66cfb1fa75f1ff892a495de2c446d5
Igor Trofimov <igor.trofimov@symmetre.com>
2023-05-20 13:08:42 +0400

    Use clientside fetch

--- src/app/secret/page.tsx
+++ src/app/secret/page.tsx
@@ -1,32 +1,34 @@
+'use client';
 import { get } from '@/lib/http';
-// import { useEffect, useState } from 'react';
-// import { LoadingOverlay } from '@/components/LoadingOverlay';
-// import { Unauthorized } from '@/components/Unauthorized';
+import { useEffect, useState } from 'react';
+import { LoadingOverlay } from '@/components/LoadingOverlay';
+import { Unauthorized } from '@/components/Unauthorized';
 import Link from 'next/link';
 
 type SecretState = {
   secret: string;
   address: string;
-  error?: { message: string }
 }
 
-async function fetchSecret(): Promise<SecretState> {
-  const res = await get('/api/v1/secret', { cache: 'no-store' });
-  if (res.status === 401) throw 'unauthorized';
+export default function SecretPage() {
+  const [data, setData] = useState<SecretState>();
+  const [error, setError] = useState('');
 
-  return await res.json();
-}
+  const init = async () => {
+    const res = await get('/api/v1/secret');
+    if (res.status !== 200) {
+      setError('Unauthorized');
+    }
+    setData(await res.json());
+  };
 
-export default async function SecretPage() {
-  // const [data, setData] = useState<SecretState>();
-  const data = await fetchSecret();
-  // useEffect(() => {
-  //   fetchSecret().then(setData);
-  // }, []);
-  //
-  // // if (!data) return <LoadingOverlay />;
-  //
-  // if (data.error) return ;
+  useEffect(() => {
+    init()
+  }, []);
+
+  if (!data) return <LoadingOverlay />;
+
+  if (error) return <Unauthorized />;
 
   return (
     <div className="m-auto max-w-3xl text-center">
